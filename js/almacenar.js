110b904114a7437b5a033cbfac14e6898aa651ed
andres71195 <asuarez71195@gmail.com>
2022-08-30 11:42:26 -0300

    2
    Avance en clase

---
+++ js/almacenar.js
@@ -0,0 +1,15 @@
+const item = document.getElementById("item");
+const buttonAgregar = document.getElementById("agregar");
+const arrayItems = 
+
+buttonAgregar.addEventListener("click", (evt) => {
+if (item.value) {
+    localStorage.setItem("item", item.value);
+}
+document.getElementById("contenedor").innerHTML = localStorage.getItem("item");
+});
+
+//Hay 3 funcionalidades diferentes, a realizar en la misma función de evento
+//Al momento de cargar la página, traer el array (almacenado en localStorage), recorrerlo con for, y cargarlo
+//localStorae solo guarda string (convertilo con stringify)
+//for of
