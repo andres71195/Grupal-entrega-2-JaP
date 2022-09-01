d009433ea080b5849c6af401523c4855680c84c4
andres71195 <asuarez71195@gmail.com>
2022-09-01 09:22:06 -0300

    3. Etiqueta <li> en innerHTML

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,12 +1,12 @@
 const item = document.getElementById("item");
 const buttonAgregar = document.getElementById("agregar");
-const arrayItems = 
+const arrayItems = [];
 
 buttonAgregar.addEventListener("click", (evt) => {
 if (item.value) {
     localStorage.setItem("item", item.value);
 }
-document.getElementById("contenedor").innerHTML = localStorage.getItem("item");
+document.getElementById("contenedor").innerHTML += '<li>'+localStorage.getItem("item")+'</li>';
 });
 
 //Hay 3 funcionalidades diferentes, a realizar en la misma función de evento
