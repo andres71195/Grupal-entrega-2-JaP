5a43f5e885264bc9e30f0d5add5e68e34af22db3
andres71195 <asuarez71195@gmail.com>
2022-09-04 22:04:17 -0300

    5
    Errores corregidos. 4/9

--- js/almacenar.js
+++ js/almacenar.js
@@ -5,7 +5,11 @@ let arrayNewItems = [];
 let arrayOldItems = [];
 
 function agregarItem() {
-    arrayNewItems = JSON.parse(localStorage.getItem("arrayItems"));
+
+    if (localStorage.getItem("arrayItems") != null) {
+        arrayNewItems = JSON.parse(localStorage.getItem("arrayItems"));
+    }
+
     if (item.value) {
         arrayNewItems.push(item.value);
         localStorage.setItem("arrayItems", JSON.stringify(arrayNewItems));
@@ -25,17 +29,12 @@ localStorage.clear();
 })
 
 document.addEventListener("DOMContentLoaded", (e) => {
+    if (localStorage.getItem("arrayItems") !== null) {
 arrayOldItems = localStorage.getItem("arrayItems");
 arrayOldItems = JSON.parse(arrayOldItems);
 for (let oldItem of arrayOldItems) {
     // console.log(oldItem);
     document.getElementById("contenedor").innerHTML += '<li>' + oldItem + '</li>';
 }
+}
 })
-
-
-
-//Hay 3 funcionalidades diferentes, a realizar en la misma función de evento
-//Al momento de cargar la página, traer el array (almacenado en localStorage), recorrerlo con for, y cargarlo
-//localStorae solo guarda string (convertilo con stringify)
-//for of
